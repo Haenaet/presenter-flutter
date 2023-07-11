e6163257dc217d079b13b1dd4d858f3354f28421
t2023-m0062 <t2023-m0062@t2023-m0062ui-MacBookAir.local>
2023-07-11 16:26:56 +0900

    feat: #7 랜덤컬러생성

--- lib/screens/memberlist.dart
+++ lib/screens/memberlist.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -15,8 +16,37 @@ class _MemberListState extends State<MemberList> {
     'LEE SE RYEONG',
     'LEE SE RYEONG',
     'LEE SE RYEONG',
-    'LEE SE RYEONG'
+    'LEE SE RYEONG',
   ];
+
+  final List<Color> colorCollection = [
+    Colors.green,
+    Colors.indigo,
+    Colors.lime,
+    Colors.pink,
+    Colors.orange,
+    Colors.purple,
+    Colors.lightBlue,
+  ];
+
+  final List<int> usedColorIndex = [];
+
+  int getRandomNumber() {
+    while (true) {
+      int randomNumber = Random().nextInt(colorCollection.length - 1);
+      if (usedColorIndex.length == profileList.length) {
+        //profileList 길이랑 같으면 break로 빠져나옴 어차피 5개만 만들거니까
+        break;
+      }
+
+      if (!usedColorIndex.contains(randomNumber)) {
+        //usedcolorindex에 randomnumber가 없으면
+        usedColorIndex.add(randomNumber); // 추가하기
+        return randomNumber;
+      }
+    }
+    return 0; //이미 모든 색을 호출하고있을땐 그냥 그린 반환. 5개만 만들거라서!!
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +56,15 @@ class _MemberListState extends State<MemberList> {
           itemCount: profileList.length,
           itemBuilder: (context, index) {
             String profile = profileList[index];
+
             return Padding(
               padding: const EdgeInsets.only(left: 10),
               child: Container(
                 alignment: Alignment.centerLeft,
                 height: 80,
                 width: 90,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
+                decoration: BoxDecoration(
+                  color: colorCollection[getRandomNumber()],
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(35),
                     bottomLeft: Radius.circular(35),

cbe2489ae4498bcfacaddc6286f167f74a738d74
Hongsik Choi <playhong16@gmail.com>
2023-07-11 17:52:23 +0900

    add: #4 이모지 및 좌우명 추가

--- lib/widgets/member_create_widget.dart
+++ lib/widgets/member_create_widget.dart
@@ -1,5 +1,75 @@
 import 'package:flutter/material.dart';
 
+class DropDownWidget extends StatefulWidget {
+  const DropDownWidget({super.key});
+
+  @override
+  State<DropDownWidget> createState() => _DropDownWidgetState();
+}
+
+class _DropDownWidgetState extends State<DropDownWidget> {
+  String _selectedEmoji = '😀';
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<String>(
+      value: _selectedEmoji,
+      decoration: const InputDecoration(
+        labelText: '이모지',
+        labelStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            width: 3,
+            color: Colors.white,
+          ),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            width: 3,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      items: const [
+        DropdownMenuItem(
+          value: '😀',
+          child: Text('😄'),
+        ),
+        DropdownMenuItem(
+          value: '😄',
+          child: Text('😄'),
+        ),
+        DropdownMenuItem(
+          value: '😊',
+          child: Text('😊'),
+        ),
+        DropdownMenuItem(
+          value: '😎',
+          child: Text('😎'),
+        ),
+        DropdownMenuItem(
+          value: '🙂',
+          child: Text('🙂'),
+        ),
+      ],
+      onChanged: (value) {
+        setState(() {
+          _selectedEmoji = value!;
+        });
+      },
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return '이모지를 선택해주세요.';
+        }
+        return null;
+      },
+    );
+  }
+}
+
 class TitleLabel extends StatelessWidget {
   const TitleLabel({
     super.key,
@@ -25,14 +95,19 @@ class CustomTextField extends StatelessWidget {
   const CustomTextField({
     super.key,
     required this.hintText,
+    this.maxLength,
+    this.maxLines,
   });
 
   final String hintText;
+  final int? maxLength;
+  final int? maxLines;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      maxLength: 34,
+      maxLength: maxLength ?? 34,
+      maxLines: maxLines ?? 1,
       cursorColor: Colors.black,
       decoration: InputDecoration(
         filled: true,
@@ -92,4 +167,70 @@ class HashTagContainer extends StatelessWidget {
       ),
     );
   }
+}
+
+class SaveButton extends StatelessWidget {
+  const SaveButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            side: const BorderSide(color: Colors.white),
+          ),
+        ),
+        backgroundColor: MaterialStateProperty.all(Colors.white),
+      ),
+      child: const Text(
+        "저장",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onPressed: () {
+        print("저장 버튼을 눌렀습니다!");
+      },
+    );
+  }
+}
+
+void showDeleteDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("삭제하시겠습니까?"),
+        actions: [
+          // 취소 버튼
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context); // 팝업 닫기
+            },
+            child: Text(
+              "취소",
+              style: TextStyle(
+                color: Colors.red,
+              ),
+            ),
+          ),
+          // 확인 버튼
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context); // 팝업 닫기
+            },
+            child: Text(
+              "확인",
+            ),
+          ),
+        ],
+      );
+    },
+  );
 }

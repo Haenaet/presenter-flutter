82a130082d08db85195c29426489d4a6c198301c
seongwon <tjddnjs549@gmail.com>
2023-07-12 11:37:12 +0900

    modified: 충돌 수정

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MaterialApp(home: MemberList());
-    );
+      return const MaterialApp(home: MemberList());
   }
 }

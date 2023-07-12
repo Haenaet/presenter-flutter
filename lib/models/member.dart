24b5bcfe88a7f913cd4e966c40a33ef90f843e65
yujinkim1 <yujin7621@me.com>
2023-07-12 21:40:59 +0900

    modified(class): Member 모델 구조변경

--- lib/models/member.dart
+++ lib/models/member.dart
@@ -1,57 +1,19 @@
-import 'package:flutter/material.dart';
-
+/// - [Member]클래스는 멤버의 정보를 가질 수 있습니다.
+/// - 포함 정보는 [name], [hashtags], [introduction], [motto], [blogAddress]입니다.
 class Member {
-
-  Member({required this.name,
-          required this.hashTag,
-          required this.introduce,
-          required this.motto, 
-          required this.blogAddress
-});
-
   final String name;
-  final List<String> hashTag;
-  final String introduce;
+  final String characterImage;
+  final List<String> hashtags;
+  final String introduction;
   final String motto;
-  dynamic blogAddress;
-}
+  final String blogAddress;
 
-class MemberService extends ChangeNotifier {
-
-  List<Map<String, dynamic>> memberList = [
-    { 'name' : 'Jin mirae' ,
-      'hashTag' : ['# entj','# 요다','#스타워즈'] ,
-      'introduce' : '안녕하세요 나는 ???입니다.' ,
-      'motto' : '열심히 하겠습니다.' ,
-      'blogAddress' : 'https://github.com/Haenaet/presenter-flutter' ,
-    },
-    { 'name' : 'Kim yujin' ,
-      'hashTag' : ['# entj','# 요다','#스타워즈'] ,
-      'introduce' : '안녕하세요 나는 ???입니다.' ,
-      'motto' : '열심히 하겠습니다.' ,
-      'blogAddress' : 'https://github.com/Haenaet/presenter-flutter' ,
-    },
-    { 'name' : 'Lee seryeong' ,
-      'hashTag' : ['# entj','# 요다','#스타워즈'] ,
-      'introduce' : '안녕하세요 나는 ???입니다.' ,
-      'motto' : '열심히 하겠습니다.' ,
-      'blogAddress' : 'https://github.com/Haenaet/presenter-flutter' ,
-    },
-    { 'name' : 'Choi hongsik' ,
-      'hashTag' : ['# entj','# 요다','#스타워즈'] ,
-      'introduce' : '안녕하세요 나는 ???입니다.' ,
-      'motto' : '열심히 하겠습니다.' ,
-      'blogAddress' : 'https://github.com/Haenaet/presenter-flutter' ,
-    },
-    { 'name' : 'Park seongwon' ,
-      'hashTag' : ['# entj','# 요다','#스타워즈'] ,
-      'introduce' : '안녕하세요 나는 ???입니다.' ,
-      'motto' : '열심히 하겠습니다.' ,
-      'blogAddress' : 'https://github.com/Haenaet/presenter-flutter' ,
-    },
-  ];
-
-
-
-  
+  Member({
+    required this.name,
+    required this.characterImage,
+    required this.hashtags,
+    required this.introduction,
+    required this.motto,
+    required this.blogAddress,
+  });
 }

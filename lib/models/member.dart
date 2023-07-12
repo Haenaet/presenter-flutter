c0a78d3f865f8ee32d9e47294057883e736a527c
yujinkim1 <yujin7621@me.com>
2023-07-13 08:39:10 +0900

    modified(class): #17 Json factory

--- lib/models/member.dart
+++ lib/models/member.dart
@@ -1,8 +1,8 @@
 /// - [Member]클래스는 멤버의 정보를 가질 수 있습니다.
-/// - 포함 정보는 [name], [hashtags], [introduction], [motto], [blogAddress]입니다.
+/// - 포함 정보는 [name], [characterImagePath], [hashtags], [introduction], [motto], [blogAddress]입니다.
 class Member {
   final String name;
-  final String characterImage;
+  final String characterImagePath;
   final List<String> hashtags;
   final String introduction;
   final String motto;
@@ -10,10 +10,32 @@ class Member {
 
   Member({
     required this.name,
-    required this.characterImage,
+    required this.characterImagePath,
     required this.hashtags,
     required this.introduction,
     required this.motto,
     required this.blogAddress,
   });
+
+  factory Member.fromJson(Map<String, dynamic> json) {
+    return Member(
+      name: json["name"],
+      hashtags: List<String>.from(json["hashtags"]),
+      introduction: json["introduction"],
+      motto: json["motto"],
+      blogAddress: json["blogAddress"],
+      characterImagePath: json["characterImagePath"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "hashtags": hashtags,
+      "introduction": introduction,
+      "motto": motto,
+      "blogAddress": blogAddress,
+      "characterImagePath": characterImagePath,
+    };
+  }
 }
